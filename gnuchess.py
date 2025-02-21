5660ef25183822cca7e814000d52c74431a59826
remi <remi.espie@etu.umontpellier.fr>
2025-02-21 12:24:15 +0100

    feat: working gnuchess python interface

---
+++ gnuchess.py
@@ -0,0 +1,39 @@
+import select
+import subprocess
+
+# Function to start GNU Chess process
+process = subprocess.Popen(
+    ['gnuchess --xboard'],
+    stdin=subprocess.PIPE,
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE,
+    shell=True,
+    text=True,
+)
+
+first_input = False
+
+# Send a command to GNU Chess
+def send_command(command):
+    process.stdin.write(command + '\n')
+    process.stdin.flush()
+
+# Read a response from GNU Chess
+def read_response(command):
+    #sleep(2)
+    while True:
+        line = process.stdout.readline()
+        if line == 'Invalid move: \n':
+            continue
+        print(line, end='')
+        if line.startswith('My move is :') or line.startswith('Invalid move: '+ command):
+            break
+    return
+
+
+# Make a move
+while True:
+    move = input("Enter a command to play a move: ")
+    send_command(move)
+    read_response(move)
+
