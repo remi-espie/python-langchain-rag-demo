964d13059f3699f9aa238fe4cc17054a40db871b
remi <remi.espie@etu.umontpellier.fr>
2025-02-21 14:36:41 +0100

    feat: have a conversation with the AI

--- ai.py
+++ ai.py
@@ -12,7 +12,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.embeddings import OllamaEmbeddings
 
-def process_input(question):
+def start_langchain():
     model_local = ChatOllama(model="phi3.5", temperature=0)
     df = pd.read_csv("club_games_data.csv")
     # Filter by really winned games
@@ -41,30 +41,53 @@ def process_input(question):
         collection_name="rag-chroma",
         embedding=embeddings.ollama.OllamaEmbeddings(model='nomic-embed-text'),
     )
-    retriever = vectorstore.as_retriever()
+    retriever = vectorstore.as_retriever(search_kwargs={"k": 1})
 
-    # Create a question / answer pipeline 
+    return model_local, retriever
+
+def new_move(question, model_local, retriever):
+    # Create a question / answer pipeline
     rag_template = """Here are lots of different chess games represented by their moves.
     {context}
-    I'm playing a game right now. I'm playing white. Here are the current game moves: {question}. 
-    What should I play next? The move must be valid. Output only the move. Output one move between quotes. For example, "e4" or "Nf3".
+    You're playing a game right now. You're playing white. Here are the current game moves: {question}. 
+    What should you play next? The move must be valid. Output only the move. Output one move between quotes. For example, "e4" or "Nf3".
     """
     rag_prompt = ChatPromptTemplate.from_template(rag_template)
     rag_chain = (
-        {"context": retriever, "question": RunnablePassthrough()}
-        | rag_prompt
-        | model_local
-        | StrOutputParser()
+            {"context": retriever, "question": RunnablePassthrough()}
+            | rag_prompt
+            | model_local
+            | StrOutputParser()
     )
 
     # Invoke the pipeline
     return rag_chain.invoke(question)
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python3 app.py '<question>'")
-        sys.exit(1)
+def first_move(model_local, retriever):
+    # Create a question / answer pipeline
+    rag_template = """Here are lots of different chess games represented by their moves.
+    {context}
+    You're playing a game right now. You're playing white.  
+    What move should you start with? The move must be valid. 
+    Output only the move. Output one move between quotes. For example, "e4" or "Nf3".
+    """
+    rag_prompt = ChatPromptTemplate.from_template(rag_template)
+    rag_chain = (
+            {"context": retriever, "question": RunnablePassthrough()}
+            | rag_prompt
+            | model_local
+            | StrOutputParser()
+    )
 
-    question = sys.argv[1]
-    answer = process_input(question)
+    # Invoke the pipeline
+    return rag_chain.invoke("")
+
+
+if __name__ == "__main__":
+    model_local, retriever = start_langchain()
+    print("----------")
+    answer = first_move(model_local, retriever)
     print(answer)
+    while True:
+        answer = new_move(input("Enter a command to play a move: "), model_local, retriever)
+        print(answer)
